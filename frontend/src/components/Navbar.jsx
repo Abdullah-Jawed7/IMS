411f5db7ed954d41b876f36c8fc3b8210686db63
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-08-27 15:20:01 +0500

    header

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <header className="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#e7edf4] px-10 py-3">
+          <div className="flex items-center gap-8">
+            <div className="flex items-center gap-4 text-[#0d141c]">
+              <div className="size-4">
+                <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M44 4H30.6666V17.3334H17.3334V30.6666H4V44H44V4Z" fill="currentColor"></path>
+                </svg>
+              </div>
+              <h2 className="text-[#0d141c] text-lg font-bold leading-tight tracking-[-0.015em]">StockPilot</h2>
+            </div>
+            {/* <div className="flex items-center gap-9">
+              <a className="text-[#0d141c] text-sm font-medium leading-normal" href="#">Dashboard</a>
+              <a className="text-[#0d141c] text-sm font-medium leading-normal" href="#">Products</a>
+              <a className="text-[#0d141c] text-sm font-medium leading-normal" href="#">Orders</a>
+              <a className="text-[#0d141c] text-sm font-medium leading-normal" href="#">Customers</a>
+              <a className="text-[#0d141c] text-sm font-medium leading-normal" href="#">Vendors</a>
+              <a className="text-[#0d141c] text-sm font-medium leading-normal" href="#">Reports</a>
+            </div> */}
+          </div>
+          <div className="flex flex-1 justify-end gap-8">
+            <label className="flex flex-col min-w-40 !h-10 max-w-64">
+              <div className="flex w-full flex-1 items-stretch rounded-xl h-full">
+                <div className="text-[#49719c] flex border-none bg-[#e7edf4] items-center justify-center pl-4 rounded-l-xl border-r-0">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
+                    <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
+                  </svg>
+                </div>
+                <input
+                  placeholder="Search"
+                  className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0d141c] focus:outline-0 focus:ring-0 border-none bg-[#e7edf4] focus:border-none h-full placeholder:text-[#49719c] px-4 rounded-l-none border-l-0 pl-2 text-base font-normal leading-normal"
+                />
+              </div>
+            </label>
+            <button className="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 bg-[#e7edf4] text-[#0d141c] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5">
+              <div className="text-[#0d141c]">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
+                  <path d="M221.8,175.94C216.25,166.38,208,139.33,208,104a80,80,0,1,0-160,0c0,35.34-8.26,62.38-13.81,71.94A16,16,0,0,0,48,200H88.81a40,40,0,0,0,78.38,0H208a16,16,0,0,0,13.8-24.06ZM128,216a24,24,0,0,1-22.62-16h45.24A24,24,0,0,1,128,216ZM48,184c7.7-13.24,16-43.92,16-80a64,64,0,1,1,128,0c0,36.05,8.28,66.73,16,80Z"></path>
+                </svg>
+              </div>
+            </button>
+            <div
+              className="bg-center bg-no-repeat aspect-square bg-cover rounded-full size-10"
+              style={{
+                backgroundImage: 'url("https://lh3.googleusercontent.com/aida-public/AB6AXuDP5JnXouprLFn0I42VSAaD03gy92pMdN7TxQPhxIXXDMmTLFAEzHIM_dePVdm9Yx_rdJk2i1JFXuF5xDf0kYJv5TF4kKDamAnYdhufRj2UAdIlzH9_oT2P2X7_070uPJ_TjVNBpAVf6DK0urzkxCvtjZQmTHQL7ExcNzQ2mJRFDm5tME23hQZJilNj4x9jliyBTnVOdaQ1Brtdmvs0HU_NrZF26Co0aJkVPKNj7vzRcB5Qarwao7-qOvWTh2iIhFA7-TQ6UZ-w9BC2")'
+              }}
+            ></div>
+          </div>
+        </header>
+  )
+}
+
+export default Navbar
